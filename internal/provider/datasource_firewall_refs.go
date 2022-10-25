cda0ac253f8a0404cbdeffef546e6c19e4fd3f02
c10l <git@c10l.cc>
2022-10-25 22:12:47 +0100

    Update data source firewall_refs

--- internal/provider/datasource_firewall_refs.go
+++ internal/provider/datasource_firewall_refs.go
@@ -1,109 +1,131 @@
 package provider
 
-// import (
-// 	"context"
-// 	"time"
+import (
+	"context"
+	"fmt"
+	"time"
 
-// 	"github.com/c10l/proxmoxve-client-go/api/cluster/firewall/refs"
-// 	"github.com/hashicorp/terraform-plugin-framework/diag"
-// 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
-// 	"github.com/hashicorp/terraform-plugin-framework/types"
-// )
+	proxmox "github.com/c10l/proxmoxve-client-go/api"
+	"github.com/c10l/proxmoxve-client-go/api/cluster/firewall/refs"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
 
-// // Ensure provider defined types fully satisfy framework interfaces
-// var _ tfsdk.DataSourceType = firewallRefsDatasourceType{}
-// var _ tfsdk.DataSource = firewallRefsDatasource{}
+// Ensure provider defined types fully satisfy framework interfaces
+var _ datasource.DataSource = &FirewallRefsDataSource{}
 
-// type firewallRefsDatasourceType struct{}
+// FirewallRefsDataSource -
+func NewFirewallRefsDataSource() datasource.DataSource {
+	return &FirewallRefsDataSource{}
+}
 
-// func (t firewallRefsDatasourceType) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
-// 	return tfsdk.Schema{
-// 		MarkdownDescription: "Lists possible IPSet/Alias reference which are allowed in source/dest properties.",
-// 		Attributes: map[string]tfsdk.Attribute{
-// 			"id": {
-// 				Type:     types.StringType,
-// 				Computed: true,
-// 			},
-// 			"type": {
-// 				Type:                types.StringType,
-// 				Optional:            true,
-// 				MarkdownDescription: "Only list references of specified type. Accepted values: `alias`, `ipset`",
-// 			},
-// 			"refs": {
-// 				Computed: true,
-// 				Attributes: tfsdk.ListNestedAttributes(map[string]tfsdk.Attribute{
-// 					"name": {
-// 						Type:     types.StringType,
-// 						Computed: true,
-// 					},
-// 					"ref": {
-// 						Type:     types.StringType,
-// 						Computed: true,
-// 					},
-// 					"type": {
-// 						Type:     types.StringType,
-// 						Computed: true,
-// 					},
-// 					"comment": {
-// 						Type:     types.StringType,
-// 						Computed: true,
-// 					},
-// 				}),
-// 			},
-// 		},
-// 	}, nil
-// }
+type FirewallRefsDataSource struct {
+	client *proxmox.Client
+}
 
-// func (t firewallRefsDatasourceType) NewDataSource(ctx context.Context, in tfsdk.Provider) (tfsdk.DataSource, diag.Diagnostics) {
-// 	provider, diags := convertProviderType(in)
+type FirewallRefsDataSourceModel struct {
+	ID   types.String                         `tfsdk:"id"`
+	Type types.String                         `tfsdk:"type"`
+	Refs []firewallRefsDataSourceDataRefModel `tfsdk:"refs"`
+}
 
-// 	return firewallRefsDatasource{
-// 		provider: provider,
-// 	}, diags
-// }
+type firewallRefsDataSourceDataRefModel struct {
+	Name    types.String `tfsdk:"name"`
+	Ref     types.String `tfsdk:"ref"`
+	Type    types.String `tfsdk:"type"`
+	Comment types.String `tfsdk:"comment"`
+}
 
-// type firewallRefsDatasourceData struct {
-// 	ID   types.String                    `tfsdk:"id"`
-// 	Type types.String                    `tfsdk:"type"`
-// 	Refs []firewallRefsDatasourceDataRef `tfsdk:"refs"`
-// }
+func (d *FirewallRefsDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_firewall_refs"
+}
 
-// type firewallRefsDatasourceDataRef struct {
-// 	Name    types.String `tfsdk:"name"`
-// 	Ref     types.String `tfsdk:"ref"`
-// 	Type    types.String `tfsdk:"type"`
-// 	Comment types.String `tfsdk:"comment"`
-// }
+func (d *FirewallRefsDataSource) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		MarkdownDescription: "Lists possible IPSet/Alias reference which are allowed in source/dest properties.",
+		Attributes: map[string]tfsdk.Attribute{
+			"id": {
+				Type:     types.StringType,
+				Computed: true,
+			},
+			"type": {
+				Type:                types.StringType,
+				Optional:            true,
+				MarkdownDescription: "Only list references of specified type. Accepted values: `alias`, `ipset`",
+			},
+			"refs": {
+				Computed: true,
+				Attributes: tfsdk.ListNestedAttributes(map[string]tfsdk.Attribute{
+					"name": {
+						Type:     types.StringType,
+						Computed: true,
+					},
+					"ref": {
+						Type:     types.StringType,
+						Computed: true,
+					},
+					"type": {
+						Type:     types.StringType,
+						Computed: true,
+					},
+					"comment": {
+						Type:     types.StringType,
+						Computed: true,
+					},
+				}),
+			},
+		},
+	}, nil
+}
 
-// type firewallRefsDatasource struct {
-// 	provider provider
-// }
+func (d *FirewallRefsDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
 
-// func (d firewallRefsDatasource) Read(ctx context.Context, req tfsdk.ReadDataSourceRequest, resp *tfsdk.ReadDataSourceResponse) {
-// 	var data firewallRefsDatasourceData
-// 	diags := req.Config.Get(ctx, &data)
-// 	resp.Diagnostics.Append(diags...)
-// 	if resp.Diagnostics.HasError() {
-// 		return
-// 	}
+	client, ok := req.ProviderData.(map[string]*proxmox.Client)["token"]
 
-// 	firewallRefs, err := refs.GetRequest{Client: d.provider.client}.Do()
-// 	if err != nil {
-// 		resp.Diagnostics.AddError("Error retrieving firewall_refs", err.Error())
-// 		return
-// 	}
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *proxmox.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
 
-// 	data.ID = types.String{Value: time.Now().String()}
-// 	data.Refs = []firewallRefsDatasourceDataRef{}
-// 	for _, v := range *firewallRefs {
-// 		ref := firewallRefsDatasourceDataRef{}
-// 		ref.Comment = types.String{Value: v.Comment}
-// 		ref.Name = types.String{Value: v.Name}
-// 		ref.Ref = types.String{Value: v.Ref}
-// 		ref.Type = types.String{Value: v.Type}
-// 		data.Refs = append(data.Refs, ref)
-// 	}
+		return
+	}
 
-// 	diags = resp.State.Set(ctx, data)
-// 	resp.Diagnostics.Append(diags...)
-// }
+	d.client = client
+}
+
+func (d *FirewallRefsDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data FirewallRefsDataSourceModel
+
+	// Read Terraform configuration data into the model
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	firewallRefs, err := refs.GetRequest{Client: d.client}.Do()
+	if err != nil {
+		resp.Diagnostics.AddError("Error retrieving firewall_refs", err.Error())
+		return
+	}
+
+	data.ID = types.String{Value: time.Now().String()}
+	data.Refs = []firewallRefsDataSourceDataRefModel{}
+	for _, v := range *firewallRefs {
+		ref := firewallRefsDataSourceDataRefModel{}
+		ref.Comment = types.String{Value: v.Comment}
+		ref.Name = types.String{Value: v.Name}
+		ref.Ref = types.String{Value: v.Ref}
+		ref.Type = types.String{Value: v.Type}
+		data.Refs = append(data.Refs, ref)
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, data)...)
+}
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -205,38 +205,9 @@ func (p *ProxmoxVEProvider) DataSources(ctx context.Context) []func() datasource
 	return []func() datasource.DataSource{
 		NewVersionDataSource,
 		NewStorageDataSource,
-		// NewFirewallRefsDataSource,
+		NewFirewallRefsDataSource,
 	}
 }
-
-// // convertProviderType is a helper function for NewResource and NewDataSource
-// // implementations to associate the concrete provider type. Alternatively,
-// // this helper can be skipped and the provider type can be directly type
-// // asserted (e.g. provider: in.(*provider)), however using this can prevent
-// // potential panics.
-// func convertProviderType(in tfsdk.Provider) (provider, diag.Diagnostics) {
-// 	var diags diag.Diagnostics
-
-// 	p, ok := in.(*provider)
-
-// 	if !ok {
-// 		diags.AddError(
-// 			"Unexpected Provider Instance Type",
-// 			fmt.Sprintf("While creating the data source or resource, an unexpected provider type (%T) was received. This is always a bug in the provider code and should be reported to the provider developers.", p),
-// 		)
-// 		return provider{}, diags
-// 	}
-
-// 	if p == nil {
-// 		diags.AddError(
-// 			"Unexpected Provider Instance Type",
-// 			"While creating the data source or resource, an unexpected empty provider instance was received. This is always a bug in the provider code and should be reported to the provider developers.",
-// 		)
-// 		return provider{}, diags
-// 	}
-
-// 	return *p, diags
-// }
 
 func New(version string) func() provider.Provider {
 	return func() provider.Provider {
