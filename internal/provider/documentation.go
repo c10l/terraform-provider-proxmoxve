1d203eb70408fe9484548bb418f08d3da53d1797
c10l <git@c10l.cc>
2022-12-21 18:04:40 +0000

    Tweak provider docs

--- internal/provider/documentation.go
+++ internal/provider/documentation.go
@@ -1,3 +1,8 @@
 package provider
 
+const providerMarkdownDescription = "" +
+	"Use the ProxmoxVE provider to manage Proxmox Virtual Environment configuration, reasources, virtual machines etc. on a compatible PMVE cluster or standalone server. You must configure the provider with API credentials before using it." +
+	"<p />The following environment variables can be set as a fallback for any omitted attributes in the provider declaration: `PROXMOXVE_BASE_URL`, `PROXMOXVE_TOKEN_ID`, `PROXMOXVE_SECRET`, `PROXMOXVE_ROOT_PASSWORD`, `PROXMOXVE_TOTPSEED`, `PROXMOXVE_TLS_INSECURE`." +
+	"<p />**NOTE:** `base_url` attribute is always required. Additionally, most API endpoints require `token_id` and `secret`. Other API endpoints require `root_password`, and if 2FA is enabled for the `root` user, `totp_seed` must also be informed."
+
 const docRequiresRoot = "<p />**NOTE:** This resource requires the provider attribute `root_password` or the environment variable `PROXMOXVE_ROOT_PASSWORD` set."
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -44,8 +44,7 @@ func (p *ProxmoxVEProvider) Metadata(ctx context.Context, req provider.MetadataR
 // GetSchema
 func (p *ProxmoxVEProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
 	resp.Schema = schema.Schema{
-		MarkdownDescription: "The following environment variables can be set as a fallback for any omitted attributes in the provider declaration: `PROXMOXVE_BASE_URL`, `PROXMOXVE_TOKEN_ID`, `PROXMOXVE_SECRET`, `PROXMOXVE_ROOT_PASSWORD`, `PROXMOXVE_TOTPSEED`, `PROXMOXVE_TLS_INSECURE`.</p>" +
-			"**NOTE:** `base_url` attribute is always required. Additionally, most API endpoints require `token_id` and `secret`. Other API endpoints require `root_password`, and if 2FA is enabled for the `root` user, `totp_seed` must also be informed.",
+		MarkdownDescription: providerMarkdownDescription,
 		Attributes: map[string]schema.Attribute{
 			"base_url": schema.StringAttribute{
 				Optional:            true,
